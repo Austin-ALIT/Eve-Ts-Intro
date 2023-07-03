9562df078dfc4a4e2b1ce6078ee0d05efa46c238
Austin-ALIT <austin@al.co.za>
2023-07-03 11:03:46 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+let message: string = 'Hello world';
+// let arrNum: Array<number> = [1,2,3];
+let arrNum: number[] = [1,2,3];
+let sett: Set<number> = new Set([1,2,3]); // Sets cannot contain duplicates
+
+
+
+// note: primitive types are lowercase. 
+// Instance types start with a capital (such as Array type)
+
+message += " again";
+console.log(message);
+
+
